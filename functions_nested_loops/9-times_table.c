bd96e8e81f1b968f267c1e298a17b0896dc20a98
root <cestrad5@gmail.com>
2022-10-14 20:00:01 -0700

    test

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,36 +1,38 @@
 #include "main.h"
 /**
-* times_table -  a function that prints the 9 times table, starting with 0
-* rone = row, cone = column, d = digits of current result
-* Return: times table
-* add extra space past single digit
+* times_table - Write a function that prints the 9 times table, starting with 0
+*
+* Return: void
 */
 void times_table(void)
 {
-int rone, cone, d;
-for (rone = 0; rone <= 9; rone++)
+int a, b, c, d, e;
+for (a = 0; a < 10; a++)
 {
+for (b = 0; b < 10; b++)
+{
+c = a * b;
+d = c / 10;
+e = c % 10;
+if (b == 0)
 _putchar('0');
-_putchar(',');
+else if (c < 10)
+{
 _putchar(' ');
-for (cone = 1; cone <= 9; cone++)
-{
-d = (rone * cone);
-if ((d / 10) > 0)
-{
-_putchar((d / 10) + '0');
+_putchar('0' + e);
 }
 else
 {
-_putchar(' ');
+_putchar('0' + d);
+_putchar('0' + e);
 }
-_putchar((d % 10) + '0');
-if (cone < 9)
+if (b < 9)
 {
 _putchar(',');
 _putchar(' ');
 }
-}
+else
 _putchar('\n');
+}
 }
 }
