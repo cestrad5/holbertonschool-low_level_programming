e57cc44aefb70771f71c0e802fc14261a77d9178
root <cestrad5@gmail.com>
2022-10-20 21:57:22 -0700

    the main.h is now completed whith all the prototipes and the funtion 1-strncat is working

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,11 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
-* File: holberton.h
-*
-* Desc: Header file containing declarations for all functions
-*       used in the 0x02-functions_nested_loops directory.
-*/
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -17,5 +11,13 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
 #endif
