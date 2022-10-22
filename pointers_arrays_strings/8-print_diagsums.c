0a1c3989088d55a53de450b3dc143e6d5139b2a7
root <cestrad5@gmail.com>
2022-10-21 19:33:26 -0700

    Uploading the full proyect

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - print sum of diagonal arr
+ * @a: array
+ * @size: size
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i, j;
+int sum1 = 0, sum2 = 0;
+
+for (i = 0; i < size; i++)
+sum1 = sum1 + a[i * (size + 1)];
+for (j = 0; j < size; j++)
+sum2 = sum2 + a[(j + 1) * (size - 1)];
+printf("%i, %i\n", sum1, sum2);
+}
