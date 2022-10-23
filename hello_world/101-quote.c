80759f758fa1db1287a4086b7d3166b73373f6f5
root <cestrad5@gmail.com>
2022-10-23 04:22:29 -0700

    finalle the new line is added at the end

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - shows a quote
-* Rerun stander error
-*/
+ *main - Entry point for the quota
+ *Return: Always 1
+ *
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
