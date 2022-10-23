59d482539e08e983eb1e95d6f0c0dea338f7df02
root <cestrad5@gmail.com>
2022-10-23 04:17:00 -0700

    finalle the new line is added at the end

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*
-*
+* main - shows a quote
+* Rerun stander error
 */
 int main(void)
 {
-  write(STDOUT_FILENO,"and that piece of art is useful \" - Dora Korpar, 2015-10-1\n", 59);
+write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
