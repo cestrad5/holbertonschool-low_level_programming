4bad92dd716df2cc2f8e0f740a8f5f3220160df6
root <cestrad5@gmail.com>
2022-10-26 19:01:14 -0700

    code fixed

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _puts_recursion - function that prints a string in reverse
+* _print_rev_recursion - function that prints a string in reverse
 * @s: string
 * Return: string
 */
