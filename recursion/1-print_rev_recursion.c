dc23616321ecc8841da947366b907b51224454f4
root <cestrad5@gmail.com>
2022-10-26 18:55:09 -0700

    code fixed

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -4,16 +4,11 @@
 * @s: string
 * Return: string
 */
-
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
+  if (*s != '\0')
 {
-_putchar('\n');
-}
-else
-{
+_print_rev_recursion(s + 1);
 _putchar(*s);
-_puts_recursion(s + 1);
 }
 }
