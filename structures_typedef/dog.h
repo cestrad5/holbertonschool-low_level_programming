bd102375720db35250cf0666c20c1169799a0904
root <cestrad5@gmail.com>
2022-11-07 17:05:37 -0800

    adding description

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+* struct dog - dog database struct
+* @*name: pointer to the dog name
+* @age: age of the dog
+* @*owner: pointer to the dog's owner name
+*/
+
 struct dog
 {
 char *name;
