31cf5d95a511bb51a4cfe407cb18944f4f462390
root <cestrad5@gmail.com>
2022-11-10 20:37:49 -0800

    solvin printf miss spelling and betty style

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,14 +1,12 @@
 #include "function_pointers.h"
-
 /**
-* int_index - Searches for an integer in an array of integers.
-* @array: The array of integers.
-* @size: The size of the array.
-* @cmp: A pointer to the function to be used to compare values.
-*
-* Return: If no element matches or size <= 0 - -1.
-*         Otherwise - The index of the first element for which
-*                     the cmp function does not return 0.
+*int_index - function that searches for an integer
+*@array: array of integers
+*@size: size of the array
+*@cmp: pointer to the function to be used to compare values
+*Return: if size <= 0, return -1
+*if  no element matches, return -1
+*otherwise return index
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
