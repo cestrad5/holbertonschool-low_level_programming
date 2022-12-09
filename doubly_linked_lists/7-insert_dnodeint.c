a24aa79d7821e46145a5c02f9e30a3aef2217a03
root <cestrad5@gmail.com>
2022-12-09 02:21:23 -0800

    kernel style solved

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index - function that inserts a new node at a given position
+* insert_dnodeint_at_index - inserts a new node at a given position
 * @h: head of the list.
 * @idx: index of the list where the new node should be added,
 * @n: number to add.
