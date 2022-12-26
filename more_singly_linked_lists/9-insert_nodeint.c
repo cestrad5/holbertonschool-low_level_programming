8d5cf32f42ca5b418093121e57bc2c8654d06413
root <cestrad5@gmail.com>
2022-12-26 04:53:51 -0800

    Compilation problem solved

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,38 +1,38 @@
 #include "lists.h"
 /**
-* insert_nodeint_at_index - inserts node at a given position
-* @head: pointer to the list head
-* @idx: index to the node to find
-* @n: value to asingned
-* Return: node
+* insert_nodeint_at_index - insert a node at index
+* @head: head of the list
+* @index: location to insert node
+* @n: value of the inserted node
+* Return: pointer to head of list
 */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-listint_t *new_node;
-listint_t *aux1 = *head;
-listint_t *aux2 = *head;
-unsigned int count;
+listint_t *current, *new;
 
-if (!*head)
+current = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
 	return (NULL);
-new_node = malloc(sizeof(listint_t));
-if (!new_node)
-	return (NULL);
-for (count = 1; count < idx; count++)
+new->n = n;
+if (index == 0)
 {
-	aux1 = aux1->next;
-	if (!aux1)
-		return (NULL);
+	new->next = current;
+	*head = new;
+	return (*head);
 }
-for (count = 1; count < (idx + 1); count++)
+while (index > 1)
 {
-	aux2  aux2->next;
-	if (!aux2)
+	current = current->next;
+	index--;
+	if (!current)
+	{
+		free(new);
 		return (NULL);
+	}
 }
-new_node->n = n;
-aux1->next = new_node;
-new_node->next = aux2;
-return (new_node);
-free(new_node);
+new->next = current->next;
+current->next = new;
+return (new);
+free(new);
 }
