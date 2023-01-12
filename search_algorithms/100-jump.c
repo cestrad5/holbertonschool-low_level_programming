b576f894b5a3908602577fb6d1a1236cbdf7d6ac
CAMILO <cestrad5@gmail.com>
2023-01-12 07:06:07 -0500

    Printed format fixed

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -14,14 +14,14 @@ int jump_search(int *array, size_t size, int value)
 
 	if (!array)
 		return (-1);
-	while (j < size)
+	while (j <= size)
 	{
 		if (j != 0)
 			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[j] >= value)
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n", i, j);
-			while (i <= j)
+			while (i < j)
 			{
 				printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 				if (array[i] == value)
